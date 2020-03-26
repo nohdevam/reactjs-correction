ee3dcbe38908241fd10ecbb83a85ab4f23645312
Noel Hoarau <n.hoarau75@gmail.com>
2020-03-26 17:42:33 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,104 +1,53 @@
 import React, { useState } from 'react';
-
 import './App.css';
-
 import Header from './component/Header/Header';
 import Content from './component/content/content';
-import MyButton from './component/MyButton/MyButton';
+import LoginForm from './component/LoginForm/LoginForm';
+import ColorsGenerator from './component/ColorsGenerator/ColorsGenerator';
+import Compteur from './component/Compteur/Compteur';
+import { data } from './assets/mockData';
 
 function App() {
-    const [ myCompteur, setMyCompteur ] = useState(0);
+    const [ formVisible, setFormVisible ] = useState(false);
+    const [ couleursVisible, setCouleursVisible ] = useState(false);
+    const [ compteurVisible, setCompteurVisible ] = useState(false);
+    const [ contentVisible, setContentVisible ] = useState(false);
 
-    const [textValue, setTextValue ] = useState('');
+    const toutCacher = () => {
+        setContentVisible(false);
+        setCompteurVisible(false);
+        setCouleursVisible(false);
+        setFormVisible(false);
+    };
 
-    const [myColor, setMyColor] = useState([]);
-    const [userName, setUserName] = useState([]);
-    const [password, setPassword] = useState([]);
-    
-    const couleur = () => {return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-};
-    const data = [
-        {
-            titre: 'News',
-            contenu: 'Lorem ipsum',
-            idItem: 1
-        },
-        {
-            titre: 'News',
-            contenu: 'Lorem ipsum',
-            idItem: 2
-        },
-        {
-            titre: 'News',
-            contenu: 'Lorem ipsum',
-            idItem: 3
-        },
-        {
-            titre: 'News',
-            contenu: 'Lorem ipsum',
-            idItem: 4
-        },
-        {
-            titre: 'News',
-            contenu: 'Lorem ipsum',
-            idItem: 5
-        },
-        {
-            titre: 'News',
-            contenu: 'Lorem ipsum',
-            idItem: 6
-        },
-        {
-            titre: 'News',
-            contenu: 'Lorem ipsum',
-            idItem: 7
-        }
-    ];
-
-    const myForm = (
-        <div>
-            <label for="nom">Entrez votre nom s'il vous plait</label>
-            <br/>
-            <input type="text" name="nom" id={userName} onChange={(e) => setUserName(e.target.value)} />
-            <br/>
-            <label>Bonjour {userName} </label>
-            <br/>
-            <label for="mdp">Entrez votre mot de passe</label>
-            <br/>
-            <input type="text"
-                name="mdp"
-                id="nom"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)} />
-            <br/>
-    <label>{password.length <=5 ? 'Le mot de passe doit comporter plus de 5 caractère' : null}</label>
-        </div>
-    )
     return (
         <div className="App">
-            <Header/>
-        
-            {myForm}
-        <div>
-            <input type='text' value={textValue} onChange={(e) => setTextValue(e.target.value)}/>
-            <p>{textValue}</p>
+            <Header
+                loginButtonOnClick={() => {
+                    toutCacher();
+                    setFormVisible(!formVisible);
+                }}
+                couleurButtonOnClick={() => {
+                    toutCacher();
+                    setCouleursVisible(!couleursVisible);
+                }}
+                compteurButtonOnClick={() => {
+                    toutCacher();
+                    setCompteurVisible(!compteurVisible);
+                }}
+                contentButtonOnClick={() => {
+                    toutCacher();
+                    setContentVisible(!contentVisible);
+                }}
+                homeButtonOnClick={toutCacher}
+            />
+
+            {formVisible ? <LoginForm /> : null}
+            {couleursVisible ? <ColorsGenerator /> : null}
+            {compteurVisible ? <Compteur /> : null}
+            {contentVisible ? <Content data={data} /> : null}
         </div>
+    );
+}
 
-            <div style={{ display:'flex',flexDirection:'row'}}>
-                <MyButton titre="Charger une couleur" myhandler={() => setMyColor([ ...myColor, couleur() ])}/> {myColor.map((item) => {
-                    return <div style={{ width:'200px', height:'300px', fontSize: 50, backgroundColor: item }}> </div>
-                })} 
-            </div>
-
-            <div style={{margin: '100px'}}>
-                <MyButton titre="Incrémenter compteur" myHandler={() => setMyCompteur(myCompteur + 1)} />
-      
-                <MyButton titre="Décrémenter compteur" myHandler={() => setMyCompteur(myCompteur - 1)} />
-            <div style={{ width: '200px', height: '200px', fontSize: 38 }}>{myCompteur}</div>
-            </div>
-            <Content data={data} />
-        </div>
-    ); 
-            }        
-
-export default App;
+export default App
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -3,14 +3,21 @@ import MyButton from '../MyButton/MyButton';
 import styles from './Header.module.css';
 
 
-const Header = (props) => {
+const Header = ({
+    loginButtonOnClick,
+    couleurButtonOnClick,
+    compteurButtonOnClick,
+    contentButtonOnClick,
+    homeButtonOnClick
+}) => {
     return (
         <header className={styles.header}>
-            <MyButton titre="Home" />
-            <MyButton titre="Contenu" />
-            <MyButton titre="Profile" />
+            <MyButton titre="Home"  myHandler={homeButtonOnClick}/>
+            <MyButton titre="Login" myHandler={loginButtonOnClick} />
+            <MyButton titre="Générateur de couleurs" myHandler={couleurButtonOnClick} />
+            <MyButton titre="Compteur" myHandler={compteurButtonOnClick}/>
+            <MyButton titre="Contenu mappé" myHandler={contentButtonOnClick}/>
         </header>
-        
     );
 };
 
